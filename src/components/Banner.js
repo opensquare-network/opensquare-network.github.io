d35f92e08bd2c8303b834af20496e063dc7b822c
Yongfeng LI <wliyongfeng@gmail.com>
2020-08-12 19:19:47 +0800

    Add typed header

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,9 +1,21 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import styled from "styled-components";
 import { BaseInner } from "./baseComponents";
+import Typed from "typed.js";
+
+const Wrapper = styled.section`
+  display: flex;
+  flex-direction: column;
+  height: calc(100vh - 73px);
+
+  h2 {
+    font-weight: normal;
+    text-align: center;
+  }
+`;
 
 const StyledSection = styled.section`
-  height: calc(100vh - 73px);
+  flex: 1;
   display: flex;
   justify-content: space-around;
   align-items: center;
@@ -29,15 +41,44 @@ export const InnerSection = styled(BaseInner)`
 `;
 
 export default function () {
+  const buildingRef = useRef(null);
+  const options = {
+    strings: [
+      "",
+      'We are building <b><i style="color: #4688F1;">crowdsourcing</i></b>',
+      'We are building <b><i style="color: #E8453C;">collaboration</i></b>',
+      'We are building <b><i style="color: #3AA757;">reputation</i></b>',
+      'We are building <b style="font-size: 36px">web3!</b>',
+    ],
+    typeSpeed: 40,
+    showCursor: false,
+  };
+
+  useEffect(() => {
+    let typed;
+    if (buildingRef.current) {
+      typed = new Typed(buildingRef.current, options);
+    }
+
+    return () => {
+      if (typed) {
+        typed.destroy();
+      }
+    };
+  }, [buildingRef, options]);
+
   return (
-    <StyledSection>
-      <InnerSection>
-        <h1>Empower Reputation Building with Blockchain </h1>
-        <h3>
-          Collaboration on bounties and your reputation score will be calculated
-          from your daily activities
-        </h3>
-      </InnerSection>
-    </StyledSection>
+    <Wrapper>
+      <h2 ref={buildingRef}>We are building</h2>
+      <StyledSection>
+        <InnerSection>
+          <h1>Empower Reputation Building with Blockchain </h1>
+          <h3>
+            Collaboration on bounties and your reputation score will be
+            calculated from your daily activities
+          </h3>
+        </InnerSection>
+      </StyledSection>
+    </Wrapper>
   );
 }
