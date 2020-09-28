ac59e21dff97a1826e7a31023f247c18a2323899
Yongfeng LI <wliyongfeng@gmail.com>
2020-09-28 22:39:56 +0800

    Refactor scores section

--- src/components/Scores/index.js
+++ src/components/Scores/index.js
@@ -1,15 +1,16 @@
 import React from "react";
 import styled from "styled-components";
+import { graphql, useStaticQuery } from "gatsby";
 import Bs from "./bs.svg";
 import Gs from "./gs.svg";
 import Fs from "./fs.svg";
+import Img from "gatsby-image";
 
 const Wrapper = styled.section`
   display: flex;
-  flex-direction: column;
-  align-items: center;
+  justify-content: center;
 
-  margin-top: 100px;
+  margin: 0 3rem;
 `;
 
 const StyledSection = styled.section`
@@ -22,7 +23,20 @@ const StyledSection = styled.section`
     padding: 0 20px;
     justify-content: space-around;
   }
+
+  div.chart {
+    margin-left: 1.5rem;
+    h2 {
+      margin: 0;
+      max-width: 440px;
+      font-weight: bold;
+      font-size: 1.75rem;
+      line-height: 1.5;
+    }
+  }
 `;
+
+const Scores = styled.section``;
 
 export const InnerSection = styled.main`
   @media screen and (min-width: 768px) {
@@ -44,52 +58,79 @@ export const InnerSection = styled.main`
   }
 `;
 
-const Item = styled.section`
+const ScoreItem = styled.section`
   display: flex;
-  flex-direction: column;
-  align-items: center;
-  width: 268px;
-
-  h3 {
-    text-align: center;
-    margin: 5px 0 0;
-    font-size: 1.5rem;
-  }
-  p {
-    text-align: center;
-    font-size: 1rem;
-    margin: 10px 0 0;
+  &:not(:first-of-type) {
+    margin-top: 2rem;
   }
 
-  @media screen and (min-width: 880px) {
-    &:not(:first-of-type) {
-      margin-left: 20px;
+  & > div {
+    margin-left: 1.8rem;
+    h4 {
+      margin: 0;
+      color: #1d253c;
+
+      font-weight: 500;
+      font-size: 1.25rem;
+      line-height: 1.6;
+    }
+
+    p {
+      margin: 0.25rem 0 0;
+      font-weight: normal;
+      font-size: 1rem;
+      line-height: 1.5;
+      color: rgba(29, 37, 60, 0.64);
+      max-width: 300px;
     }
   }
 `;
 
 export default function () {
+  const data = useStaticQuery(graphql`
+    query {
+      scoreImage: file(relativePath: { eq: "score.png" }) {
+        childImageSharp {
+          fixed(width: 420) {
+            ...GatsbyImageSharpFixed
+          }
+        }
+      }
+    }
+  `);
+
   return (
     <Wrapper>
-      <h2>Reputation scores help you find best collaborators</h2>
       <StyledSection>
-        <InnerSection>
-          <Item>
-            <Bs width={100} height={100} />
-            <h3>Behavior Score</h3>
-            <p>Reputation with users' behaviors, no scope limitation</p>
-          </Item>
-          <Item>
-            <Gs width={100} height={100} />
-            <h3>General Score</h3>
-            <p>Normalization of total behavior reputation score, in (0, 100)</p>
-          </Item>
-          <Item>
-            <Fs width={100} height={100} />
-            <h3>Feature Scores</h3>
-            <p>Reputation with customized behavior set</p>
-          </Item>
-        </InnerSection>
+        <Scores>
+          <ScoreItem>
+            <Bs />
+            <div>
+              <h4>Behavior Score</h4>
+              <p>Reputation with users' behaviors, no range limitation</p>
+            </div>
+          </ScoreItem>
+          <ScoreItem>
+            <Gs />
+            <div>
+              <h4>General Score</h4>
+              <p>
+                Normalization of total behavior reputation score, in (0, 100)
+              </p>
+            </div>
+          </ScoreItem>
+          <ScoreItem>
+            <Fs />
+            <div>
+              <h4>Feature Score</h4>
+              <p>Reputation with customized behavior set</p>
+            </div>
+          </ScoreItem>
+        </Scores>
+        <div className="chart">
+          <h2>Reputation scores help you find best collaborators</h2>
+          <Img fixed={data.scoreImage.childImageSharp.fixed} />
+        </div>
       </StyledSection>
     </Wrapper>
   );
