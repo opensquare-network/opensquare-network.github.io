8c39b091aa15c3e3f8167efdc6fcc4e9a5cf387a
Yongfeng LI <wliyongfeng@gmail.com>
2020-09-28 17:58:03 +0800

    Refactor Participator section

--- src/components/Participator/index.js
+++ src/components/Participator/index.js
@@ -1,32 +1,52 @@
 import React from "react";
 import styled from "styled-components";
-import Funders from "./funders.svg";
-import Hunters from "./hunters.svg";
-import Council from "./council-dot.svg";
+import Council from "./council.svg";
+import Funder from "./funder.svg";
+import Hunter from "./hunter.svg";
+import Line from "./line.svg";
+import Square from "./square-bg.svg";
 
 const Wrapper = styled.section`
   display: flex;
   flex-direction: column;
   align-items: center;
+  position: relative;
 
-  margin-top: 100px;
+  background: #1d253c;
+
+  margin-top: 4rem;
+  padding: 3.3rem 0;
+
+  overflow: hidden;
 `;
 
 const StyledSection = styled.section`
-  background: #ffffff;
   display: flex;
+  flex-direction: column;
   justify-content: space-around;
+  width: 100%;
 
   @media screen and (max-width: 840px) {
     padding: 0 20px;
     justify-content: space-around;
+  }
+
+  @media screen and (min-width: 1080px) {
+    width: 1080px;
+  }
+
+  h2 {
+    font-size: 1.75rem;
+    line-height: 1.5;
+    color: #fff;
+    margin: 0 0 2rem;
   }
 `;
 
 export const InnerSection = styled.main`
   @media screen and (min-width: 768px) {
     display: flex;
-    justify-content: space-around;
+    justify-content: flex-start;
   }
 
   @media screen and (min-width: 1080px) {
@@ -43,52 +63,61 @@ export const InnerSection = styled.main`
   }
 `;
 
-const Item = styled.section`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  width: 268px;
+export const Item = styled.section`
+  & > div {
+    display: flex;
+    align-items: center;
+
+    & > svg:last-of-type {
+      margin: 0 3rem;
+    }
+  }
 
   h3 {
-    text-align: center;
-    margin: 5px 0 0;
-    font-size: 1.5rem;
-  }
-  p {
-    text-align: center;
-    font-size: 1rem;
-    margin: 10px 0 0;
+    font-weight: 500;
+    font-size: 1.25rem;
+    line-height: 2rem;
+    color: #fff;
+    margin: 0.75rem 0 0.25rem;
   }
 
-  @media screen and (min-width: 880px) {
-    &:not(:first-of-type) {
-      margin-left: 20px;
-    }
+  p {
+    color: rgba(255, 255, 255, 0.48);
+    font-size: 1rem;
+    line-height: 1.75rem;
+    margin: 0;
+    max-width: 14rem;
   }
 `;
 
 export default function () {
   return (
     <Wrapper>
-      <h2>Collaborations keep the network active</h2>
       <StyledSection>
+        <h2>Collaborations keep the network active</h2>
         <InnerSection>
           <Item>
-            <Funders width={100} height={100} />
+            <div>
+              <Funder />
+              <Line />
+            </div>
             <h3>Funders</h3>
-            <p>Fund bounties, review hunters' work</p>
+            <p>Fund bounties, Review hunters' work</p>
           </Item>
           <Item>
-            <Council width={100} height={100} />
+            <div>
+              <Council />
+              <Line />
+            </div>
             <h3>Council</h3>
             <p>Review bounties, judge disputes, governance</p>
           </Item>
           <Item>
-            <Hunters width={100} height={100} />
+            <Hunter />
             <h3>Hunters</h3>
-            <p>Apply bounties, solve them</p>
-            <p></p>
+            <p>Apply bounties, Solve them</p>
           </Item>
+          <Square style={{ position: "absolute", right: "-19rem", top: 0 }} />
         </InnerSection>
       </StyledSection>
     </Wrapper>
