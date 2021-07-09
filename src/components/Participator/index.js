4dccb2551300d861578da4473b651b778958cfa4
yoshiyuki <brother-zhou@qq.com>
2021-07-09 14:14:30 +0800

    feature: network

--- src/components/Participator/index.js
+++ src/components/Participator/index.js
@@ -5,10 +5,10 @@ import React from "react";
 export default function () {
   return (
     <div className="py-20 bg-cta">
-      <h3 className="text-center text-xl w-266px mx-auto text-bold text-white ">
+      <h3 className="text-center text-xl md:text-4xl w-266px md:w-full mx-auto text-bold text-white ">
         A trusted collaboration network
       </h3>
-      <p className="text-gray-400 w-350px mx-auto mt-4">
+      <p className="text-center text-gray-400 w-350px md:w-476px mx-auto mt-4">
         Consequat gravida morbi rhoncus sit. Nisl, platea ligula pellentesque
         nunc arcu commodo consequat. Pellentesque consectetur elit morbi
         porttitor aliquet.
