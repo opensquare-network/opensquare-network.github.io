a1039d3fee26b244243a4111694dc6c1b85570d6
yoshiyuki <brother-zhou@qq.com>
2021-07-13 12:21:17 +0800

    stylefix: fix Home page pruducts section

--- src/components/Blocks/ProductSlogan.js
+++ src/components/Blocks/ProductSlogan.js
@@ -1,31 +1,6 @@
 import React from "react";
 import styled from "styled-components";
 
-const InnerSection = styled.section`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-
-  h3,
-  p {
-    margin: 0;
-  }
-
-  @media screen and (min-width: 1080px) {
-    width: 1080px;
-  }
-`;
-
-// const Title = styled.h3`
-//   text-align: center;
-//   min-width: 204px;
-//   font-weight: 600;
-//   font-size: 1.75rem;
-//   color: #1d253c;
-//   letter-spacing: 0.2px;
-//   line-height: 1.5;
-// `;
-
 const Detail = styled.p`
   font-size: 1rem;
   color: rgba(29, 37, 60, 0.64);
@@ -36,13 +11,11 @@ const Detail = styled.p`
 
 export default function ProductSlogan() {
   return (
-    <InnerSection>
-      <h3 className="title text-3xl font-bold text-dark-major  font-mont">
+    <div>
+      <h3 className="h4-24-bold md:h3-36-bold text-dark-major mb-1 md:mb-4">
         We build for collaboration
       </h3>
       <Detail>Molestie eget lacus nisi, aliquet tincidunt.</Detail>
-
-      {/*</style>*/}
-    </InnerSection>
+    </div>
   );
 }
--- src/components/Blocks/Products.js
+++ src/components/Blocks/Products.js
@@ -6,87 +6,93 @@ import Employ from "./employ.png";
 
 export default function Products() {
   return (
-    <div>
-      <div className="flex flex-wrap pl-9	pt-16 content-center justify-center w-full md:w-1080px font-inter">
-        <div
-          className="
-          relative pt-6 pl-15 mb-68px ml-10 sm:ml-0 mr-8
+    <div className="flex flex-wrap pl-9 content-center justify-center w-full md:w-1080px font-inter  mt-8 pt-9 break-all">
+      <div
+        className="
+          relative pt-6 pl-15 pr-6 mb-68px  sm:ml-0 mr-8
           sm:mr-76px w-346px sm:w-484px h-160px
           sm:h-136px shadow-lg sm:even:mt-78px
           bg-white text-dark-major
           "
-        >
-          <img
-            alt=""
-            src={CoinAsk}
-            className="absolute -top-9 -left-9 shadow-lg"
-          />
-          <h3 className="text-xl font-bold mb-2">Pay to Ask</h3>
-          <p className="text-dark-minor">
-            Tortor etiam duis diam est, massa nullam. Mauris enim lorem arcu
-            euismod pulvinar nisl.
-          </p>
-        </div>
+      >
+        <img
+          width={72}
+          height={72}
+          alt=""
+          src={CoinAsk}
+          className="absolute p-4 bg-white -top-9 -left-9 shadow-lg"
+        />
+        <p className="p-20-bold mb-2">Pay to Ask</p>
+        <p className="text-dark-minor">
+          Tortor etiam duis diam est, massa nullam. Mauris enim lorem arcu
+          euismod pulvinar nisl.
+        </p>
+      </div>
 
-        <div
-          className="
-          relative pt-6 pl-15 mb-68px ml-10 sm:ml-0 mr-8
+      <div
+        className="
+          relative pt-6 pl-15 pr-6 mb-68px  sm:ml-0 mr-8
           sm:mr-0 w-346px sm:w-484px h-160px
           sm:h-136px shadow-lg sm:even:mt-78px
           bg-white
           "
-        >
-          <img
-            alt=""
-            src={Bounty}
-            className="absolute -top-9 -left-9 shadow-lg"
-          />
-          <h3 className="text-xl font-bold mb-2">Bounties</h3>
-          <p className="text-dark-minor">
-            Nec mauris sed ligula vivamus scelerisque gravida diam. Porttitor id
-            elementum nam libero.
-          </p>
-        </div>
+      >
+        <img
+          width={72}
+          height={72}
+          alt=""
+          src={Bounty}
+          className="absolute p-4 bg-white  -top-9 -left-9 shadow-lg"
+        />
+        <p className="p-20-bold mb-2">Bounties</p>
+        <p className="text-dark-minor">
+          Nec mauris sed ligula vivamus scelerisque gravida diam. Porttitor id
+          elementum nam libero.
+        </p>
+      </div>
 
-        <div
-          className="
-          relative pt-6 pl-15 mb-68px ml-10 sm:ml-0 mr-8
+      <div
+        className="
+          relative pt-6 pl-15 pr-6 mb-68px  sm:ml-0 mr-8
           sm:mr-76px w-346px sm:w-484px h-160px
           sm:h-136px shadow-lg sm:even:mt-78px
           bg-white
           "
-        >
-          <img
-            alt=""
-            src={Employ}
-            className="absolute -top-9 -left-9 shadow-lg"
-          />
-          <h3 className="text-xl font-bold mb-2">Short-term Employment</h3>
-          <p className="text-dark-minor">
-            Tortor etiam duis diam est, massa nullam. Mauris enim lorem arcu
-            euismod pulvinar nisl.
-          </p>
-        </div>
+      >
+        <img
+          width={72}
+          height={72}
+          alt=""
+          src={Employ}
+          className="absolute p-4 bg-white  -top-9 -left-9 shadow-lg"
+        />
+        <p className="p-20-bold mb-2">Short-term Employment</p>
+        <p className="text-dark-minor">
+          Tortor etiam duis diam est, massa nullam. Mauris enim lorem arcu
+          euismod pulvinar nisl.
+        </p>
+      </div>
 
-        <div
-          className="
-          relative pt-6 pl-15 mb-68px ml-10 sm:ml-0 mr-8
+      <div
+        className="
+          relative pt-6 pl-15 pr-6 mb-68px  sm:ml-0 mr-8
           sm:mr-0 w-346px sm:w-484px h-160px
           sm:h-136px shadow-lg sm:even:mt-78px
           bg-white
           "
-        >
-          <img
-            alt=""
-            src={Collaboration}
-            className="absolute -top-9 -left-9 shadow-lg"
-          />
-          <h3 className="text-xl font-bold mb-2">Custom Collaboration</h3>
-          <p className="text-dark-minor">
-            Nec mauris sed ligula vivamus scelerisque gravida diam. Porttitor id
-            elementum nam libero.
-          </p>
-        </div>
+      >
+        <img
+          width={72}
+          height={72}
+          alt=""
+          src={Collaboration}
+          className="absolute p-4 bg-white  -top-9 -left-9 shadow-lg"
+        />
+        <p className="p-20-bold mb-2">Custom Collaboration</p>
+        <p className="text-dark-minor">
+          Nec mauris sed ligula vivamus scelerisque gravida diam. Porttitor id
+          elementum nam libero.
+        </p>
       </div>
     </div>
   );
