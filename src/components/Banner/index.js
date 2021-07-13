96f6d620d355251dd4ea80df85710333bbdee656
yoshiyuki <brother-zhou@qq.com>
2021-07-13 11:58:31 +0800

    stylefix: fix Home page hero section

--- src/components/Banner/index.js
+++ src/components/Banner/index.js
@@ -27,7 +27,7 @@ export default function () {
       <StyledSection className="h-476px md:h-576px">
         <div className="w-1080px">
           <div className="block md:flex">
-            <h1 className="h3-36-bold md:h1-60-bold text-center">
+            <h1 className="h3-36-bold md:h1-60-bold text-center text-dark-major">
               {/*pb-5 md:pb-6 text-4xl leading-tight text-center md:text-left text-mark-major font-bold*/}
               Empower &nbsp;
             </h1>
@@ -35,7 +35,7 @@ export default function () {
               <ColorText>Credit Building</ColorText>
             </h1>
           </div>
-          <h1 className="h3-36-bold md:h1-60-bold text-center md:text-left mt-3 md:mt-4">
+          <h1 className="h3-36-bold md:h1-60-bold text-center md:text-left mt-3 md:mt-4 text-dark-major">
             with Blockchain
           </h1>
           <p className="p-20-normal w-346px md:w-full mx-auto md:text-left mt-4 md:mt-6 mb-12 md:mb-16 text-dark-minor text-center md:text-left">
