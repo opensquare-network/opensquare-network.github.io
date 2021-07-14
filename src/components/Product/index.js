d421c12acb7f1760b29ef293ef13c70865b002ab
yoshiyuki <brother-zhou@qq.com>
2021-07-14 20:03:03 +0800

    wording

--- src/components/Product/index.js
+++ src/components/Product/index.js
@@ -125,12 +125,12 @@ export default function Product() {
               <ExternalLink className="inline" />
             </a>
             <p className="border-t border-grey-200 mt-3 pt-2 flex">
-              <a className="mr-6" href="/">
+              <span className="mr-6" relative bg-white>
                 <KusamaTreasury />
-              </a>
-              <a href="/">
+              </span>
+              <span relative bg-white>
                 <StateMintTreasury />
-              </a>
+              </span>
             </p>
           </div>
 
@@ -156,9 +156,9 @@ export default function Product() {
               <ExternalLink className="inline" />
             </a>
             <p className="border-t  border-grey-200  mt-3 pt-2">
-              <a href="/">
+              <span relative bg-white>
                 <KusamaTreasury />
-              </a>
+              </span>
             </p>
           </div>
         </div>
