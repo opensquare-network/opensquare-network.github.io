d4cbc69a790a1bea88106b8cc39c1d555318c362
yoshiyuki <brother-zhou@qq.com>
2021-07-15 18:07:46 +0800

    fix: tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,11 @@ module.exports = {
   theme: {
     screens: {
       smsm: "414px",
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1536px",
     },
     fontFamily: {
       inter: ["inter", "sans-serif"],
