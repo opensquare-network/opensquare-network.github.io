a2b9c60628d3b5f8444d9c5d918ffd992da34e7c
yoshiyuki <brother-zhou@qq.com>
2021-07-19 09:29:53 +0800

    improve: responsive for middle sizes

--- src/components/Blocks/Steps.js
+++ src/components/Blocks/Steps.js
@@ -4,11 +4,11 @@ import IconY from "./blocksY.png";
 
 export default function Steps() {
   return (
-    <div className="w-full md:w-1080px md:m-auto md:flex items-start">
-      <div className="w-full mt-8 md:mt-12 pt-4 md:w-60 px-4 md:px-0 flex justify-center md:justify-between md:flex-wrap   md:mb-0  md:mr-10">
-        <img alt="" src={IconY} className="md:hidden h-32" />
-        <img alt="" src={Icon} className="hidden md:block md:mb-9 h-44px" />
-        <div className="w-266px ml-10 md:ml-0">
+    <div className="w-full lg:w-1080px lg:m-auto lg:flex items-start">
+      <div className="w-full mt-8 lg:mt-12 pt-4 lg:w-60 px-4 lg:px-0 flex justify-center lg:justify-between lg:flex-wrap   lg:mb-0  lg:mr-10">
+        <img alt="" src={IconY} className="lg:hidden h-32" />
+        <img alt="" src={Icon} className="hidden lg:block lg:mb-9 h-44px" />
+        <div className="w-266px ml-10 lg:ml-0">
           <h3 className="text-xl font-bold mb-4">Task Releasing</h3>
           <p className="text-base font-normal text-dark-minor">
             Funder issues a request, hunters apply for it.
@@ -16,10 +16,10 @@ export default function Steps() {
         </div>
       </div>
 
-      <div className="w-full  mt-8 md:mt-12 pt-4 md:w-60 px-4 md:px-0 flex justify-center md:justify-between md:flex-wrap   md:mb-0  md:mr-10">
-        <img alt="" src={IconY} height={128} className="md:hidden h-32" />
-        <img alt="" src={Icon} className="hidden md:block md:mb-9 h-44px" />
-        <div className="w-266px ml-10 md:ml-0">
+      <div className="w-full  mt-8 lg:mt-12 pt-4 lg:w-60 px-4 lg:px-0 flex justify-center lg:justify-between lg:flex-wrap   lg:mb-0  lg:mr-10">
+        <img alt="" src={IconY} height={128} className="lg:hidden h-32" />
+        <img alt="" src={Icon} className="hidden lg:block lg:mb-9 h-44px" />
+        <div className="w-266px ml-10 lg:ml-0">
           <h3 className="text-xl font-bold mb-4">Data Checking</h3>
           <p className="text-base font-normal text-dark-minor">
             Funder check candidates' profile, historic collaborations and
@@ -28,10 +28,10 @@ export default function Steps() {
         </div>
       </div>
 
-      <div className="w-full mt-8 md:mt-12 pt-4  md:w-60 px-4 md:px-0 flex justify-center md:justify-between md:flex-wrap   md:mb-0  md:mr-10">
-        <img alt="" src={IconY} height={128} className="md:hidden h-32" />
-        <img alt="" src={Icon} className="hidden md:block md:mb-9 h-44px" />
-        <div className="w-266px ml-10 md:ml-0">
+      <div className="w-full mt-8 lg:mt-12 pt-4  lg:w-60 px-4 lg:px-0 flex justify-center lg:justify-between lg:flex-wrap   lg:mb-0  lg:mr-10">
+        <img alt="" src={IconY} height={128} className="lg:hidden h-32" />
+        <img alt="" src={Icon} className="hidden lg:block lg:mb-9 h-44px" />
+        <div className="w-266px ml-10 lg:ml-0">
           <h3 className="text-xl font-bold mb-4">Task Compeletion</h3>
           <p className="text-base font-normal text-dark-minor">
             Be assigned, hunters should deliver it on time and maybe report it
@@ -40,10 +40,10 @@ export default function Steps() {
         </div>
       </div>
 
-      <div className="w-full  mt-8 md:mt-12 pt-4 md:w-60 px-4 md:px-0 flex justify-center md:justify-between md:flex-wrap   md:mb-0  md:mr-10">
-        <img alt="" src={IconY} height={128} className="md:hidden h-32" />
-        <img alt="" src={Icon} className="hidden md:block md:mb-9 h-44px" />
-        <div className="w-266px ml-10 md:ml-0">
+      <div className="w-full  mt-8 lg:mt-12 pt-4 lg:w-60 px-4 lg:px-0 flex justify-center lg:justify-between lg:flex-wrap   lg:mb-0  lg:mr-10">
+        <img alt="" src={IconY} height={128} className="lg:hidden h-32" />
+        <img alt="" src={Icon} className="hidden lg:block lg:mb-9 h-44px" />
+        <div className="w-266px ml-10 lg:ml-0">
           <h3 className="text-xl font-bold mb-4">Credit and Incentive</h3>
           <p className="text-base font-normal text-dark-minor">
             Credits built by the subjective comments and objective behaviors,
