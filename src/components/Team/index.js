6dfb84afaa7dcece392582089349854d1e0121b4
yoshiyuki <634554815@qq.com>
2021-10-13 23:01:31 +0800

    staff turnover (#26)

--- src/components/Team/index.js
+++ src/components/Team/index.js
@@ -168,86 +168,8 @@ export default function () {
               passion for the product design and design system. Figma fan.
             </p>
           </div>
-
-          <div className="px-4 mr-10 lg:mr-0 mb-8 lg:mb-80px  md:h-388px md:flex content-start w-full md:w-240px flex-wrap">
-            <img
-              className="mx-auto mb-6 md:ml-0"
-              width={100}
-              src={Yaping}
-              alt=""
-            />
-            <p className="md:w-full text-center md:text-left text-lg font-semibold">
-              Yaping
-            </p>
-            <p className="md:w-full text-center md:text-left text-base font-semibold text-turquoise-500 mb-3 ">
-              BD & Operation
-            </p>
-            <p className="md:w-full flex justify-center md:justify-start mb-4">
-              <Contacts email="yaping@opensquare.network" />
-            </p>
-            <p className="text-dark-minor font-normal w-360px md:w-full text-center md:text-left m-auto">
-              Yaping, passionate about Blockchain. Intergrating her media
-              experience with commercial mindset in this trendy industry to
-              collaborate the decentralized future. Reading and outdoor. Logic,
-              abstract and intuitive.
-            </p>
-          </div>
         </div>
       </div>
-
-      {/*<div className="block md:flex md:mt-40 md:w-1080px md:mx-auto md:items-start">*/}
-      {/*  <h2*/}
-      {/*    className="h4-24-bold*/}
-      {/*   text-dark-major*/}
-      {/*  text-center  mt-28  mb-8*/}
-      {/*  md:mt-0 md:text-left md:mr-133px*/}
-      {/*  "*/}
-      {/*  >*/}
-      {/*    Carrers*/}
-      {/*  </h2>*/}
-
-      {/*  <div>*/}
-      {/*    <p className="px-4 p-16-normal text-dark-minor mb-4 md:w-800px">*/}
-      {/*      We aim to calm this mess by providing a simple user interface that*/}
-      {/*      reduces tools to their core functionality. OpenSquare is designed to*/}
-      {/*      keep their users focused. It optimizes for the shortest path from*/}
-      {/*      intent to action.*/}
-      {/*    </p>*/}
-      {/*    <p className="px-4  p-16-normal text-dark-minor mb-4">*/}
-      {/*      If you resonate with our mission but there isn’t an open position*/}
-      {/*      for you, you can also send us an email at{" "}*/}
-      {/*      <a*/}
-      {/*        className="text-turquoise-500"*/}
-      {/*        href="mailto:yongfeng@opensquare.com"*/}
-      {/*      >*/}
-      {/*        yongfeng@opensquare.com*/}
-      {/*      </a>{" "}*/}
-      {/*      with any information you’d think is relevant*/}
-      {/*    </p>*/}
-
-      {/*    <h3 className="mx-4 p-20-bold mt-10 pt-10 border-t border-grey-200 text-2xl text-dark-major text-left font-bold mb-8">*/}
-      {/*      Operations Manager*/}
-      {/*    </h3>*/}
-
-      {/*    <p className="mx-4 p-16-normal text-dark-minor mb-4">*/}
-      {/*      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Imperdiet*/}
-      {/*      diam placerat fusce nullam malesuada ipsum elit. At magna est*/}
-      {/*      mauris, egestas et. Consectetur mi lacus semper lectus. Quis in ut*/}
-      {/*      sed est eleifend velit magna adipiscing iaculis.*/}
-      {/*    </p>*/}
-
-      {/*    <h3 className="mx-4 p-20-bold mt-10 pt-10 border-t border-grey-200 text-2xl text-dark-major text-left font-bold mb-8">*/}
-      {/*      Blockchain Developer*/}
-      {/*    </h3>*/}
-
-      {/*    <p className="px-4 p-16-normal mb-20 text-dark-minor mb-4">*/}
-      {/*      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Enim,*/}
-      {/*      pharetra suspendisse duis dapibus nunc in. Non felis erat feugiat mi*/}
-      {/*      sed. Nullam eu aliquet magna turpis eu montes, et congue. Nunc enim,*/}
-      {/*      in risus eget euismod turpis.*/}
-      {/*    </p>*/}
-      {/*  </div>*/}
-      {/*</div>*/}
     </div>
   );
 }
