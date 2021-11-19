6b70ba6e84137fb705dee7355593ea837b520e95
qiyisi <qiyisi@qq.com>
2021-11-19 18:37:20 +0800

    Update ui, #37

--- src/components/Product/index.js
+++ src/components/Product/index.js
@@ -6,6 +6,7 @@ import Odes from "./odes.png";
 import StateScan from "./statescan.png";
 import Bounty from "./bounties.png";
 import Vote from "./votes.png";
+import Subsquare from "./subsquare.png";
 
 import ExternalLink from "./externalLink.svg";
 import KusamaTreasury from "./kusamaTreasury.svg";
@@ -17,14 +18,12 @@ export default function Product() {
   return (
     <div className="font-inter lg:mb-20">
       <div className="w-full bg-white">
-        <div className="lg:h-288px lg:w-1080px lg:mx-auto pt-20 bg-white">
+        <div className="lg:h-236px lg:w-1080px lg:mx-auto pt-20 bg-white">
           <div className="lg:w-1080px m-auto lg:flex mb-4">
-            <h1 className="h3-36-bold lg:h2-48-bold text-center mb-3 lg:mb-0">
+            <h1 className="h3-36-bold text-center mb-3 lg:mb-0">
               We build for
             </h1>
-            <h1 className="h3-36-bold lg:h2-48-bold text-center">
-              &nbsp;collaborations
-            </h1>
+            <h1 className="h3-36-bold text-center">&nbsp;collaborations</h1>
           </div>
           <p
             className="
@@ -49,13 +48,13 @@ export default function Product() {
           Collaboration
         </h2>
         <div className=" lg:flex lg:flex-wrap lg:mt-9">
-          <div className="relative bg-white w-286px sm:w-346px m-auto pt-6 pr-7 pl-15 pb-6 shadow-lg mb-16 lg:mb-0">
+          <div className="relative bg-white w-286px sm:w-346px m-auto pt-6 pr-7 pl-15 pb-6 shadow-lg mb-16 lg:mb-0 border border-grey-200">
             <img
               src={Bounty}
               alt=""
-              className="w-72px p-4 shadow bg-white absolute -top-9 -left-9 "
+              className="w-72px p-4 shadow bg-white absolute -top-9 -left-9 border border-grey-200"
             />
-            <p className="lg:w-full text-left  lg:text-left p-20-bold mb-2">
+            <p className="lg:w-full text-left  lg:text-left p-20-medium mb-2">
               OpenSquare Bounties
             </p>
             <p className="text-dark-minor p-16-normal text-left">
@@ -71,26 +70,51 @@ export default function Product() {
             </p>
           </div>
 
-          <div className="relative bg-white w-286px sm:w-346px lg:ml-76px m-auto pt-6 pr-7 pl-15 pb-6 shadow-lg">
+          <div className="border border-grey-200 relative bg-white w-286px sm:w-346px lg:ml-76px m-auto pt-6 pr-7 pl-15 pb-6 shadow-lg">
             <img
               src={Vote}
               alt=""
-              className="w-72px p-4 shadow bg-white absolute -top-9 -left-9 "
+              className="w-72px p-4 shadow bg-white absolute -top-9 -left-9 border border-grey-200"
             />
-            <p className="lg:w-full text-left  lg:text-left p-20-bold mb-2">
+            <p className="lg:w-full text-left  lg:text-left p-20-medium mb-2">
               OpenSquare Votes
             </p>
             <p className="text-dark-minor p-16-normal text-left">
               An off-chain voting system for Polkadot ecosystem.
             </p>
-            <p className="mt-3 text-right">
-              <span
-                className="inline-block px-3 h-6 bg-grey-300 text-white  p-12-bold"
-                style={{ lineHeight: "24px" }}
-              >
-                Coming Soon
-              </span>
+            <a
+              className="block text-right"
+              href="https://www.opensquare.io/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <ExternalLink className="inline" />
+            </a>
+          </div>
+
+          <div
+            className=" border border-grey-200 relative bg-white w-286px sm:w-346px m-auto pt-6 pr-7 pl-15 pb-6 shadow-lg mt-76px"
+            style={{ marginLeft: "31.6875px" }}
+          >
+            <img
+              src={Subsquare}
+              alt=""
+              className="w-72px p-4 shadow bg-white absolute -top-9 -left-9 border border-grey-200"
+            />
+            <p className="lg:w-full text-left  lg:text-left p-20-medium mb-2">
+              SubSquare
             </p>
+            <p className="text-dark-minor p-16-normal text-left">
+              A governance platform for substrate based chains.
+            </p>
+            <a
+              className="block text-right"
+              href="https://www.subsquare.io/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <ExternalLink className="inline" />
+            </a>
           </div>
         </div>
       </Row>
@@ -100,13 +124,13 @@ export default function Product() {
           Explorer
         </h2>
         <div className="lg:flex lg:flex-wrap lg:mt-9">
-          <div className="relative bg-white w-286px sm:w-346px h-233px m-auto pt-6 pr-7 pl-15 pb-6 shadow-lg mb-16 lg:mb-0">
+          <div className="border border-grey-200 relative bg-white w-286px sm:w-346px h-233px m-auto pt-6 pr-7 pl-15 pb-6 shadow-lg mb-16 lg:mb-0">
             <img
               src={Dotreasury}
               alt=""
-              className="w-72px p-4 shadow bg-white absolute -top-9 -left-9 "
+              className="w-72px p-4 shadow bg-white absolute -top-9 -left-9 border border-grey-200"
             />
-            <p className="lg:w-full text-left  lg:text-left p-20-bold mb-2">
+            <p className="lg:w-full text-left  lg:text-left p-20-medium mb-2">
               doTreasury
             </p>
             <p className="text-dark-minor p-16-normal text-left">
@@ -131,13 +155,13 @@ export default function Product() {
             </p>
           </div>
 
-          <div className="relative bg-white w-286px sm:w-346px  h-233px lg:ml-76px m-auto pt-6 pr-7 pl-15 pb-6 shadow-lg">
+          <div className="border border-grey-200 relative bg-white w-286px sm:w-346px  h-233px lg:ml-76px m-auto pt-6 pr-7 pl-15 pb-6 shadow-lg">
             <img
               src={StateScan}
               alt=""
-              className="absolute w-72px p-4 bg-white shadow -top-9 -left-9 "
+              className="absolute w-72px p-4 bg-white shadow -top-9 -left-9 border border-grey-200"
             />
-            <p className="lg:w-full text-left  lg:text-left p-20-bold mb-2">
+            <p className="lg:w-full text-left  lg:text-left p-20-medium mb-2">
               Statescan
             </p>
             <p className="text-dark-minor p-16-normal text-left">
@@ -168,13 +192,13 @@ export default function Product() {
           Platform
         </h2>
         <div className="lg:flex lg:flex-wrap lg:mt-9">
-          <div className="relative bg-white w-286px sm:w-346px m-auto pt-6 pr-7 pl-15 pb-6 shadow-lg mb-16 lg:mb-0">
+          <div className="border border-grey-200 relative bg-white w-286px sm:w-346px m-auto pt-6 pr-7 pl-15 pb-6 shadow-lg mb-16 lg:mb-0">
             <img
               src={CoinAsk}
               alt=""
-              className="w-72px p-4 shadow bg-white absolute -top-9 -left-9 "
+              className="w-72px p-4 shadow bg-white absolute -top-9 -left-9 border border-grey-200"
             />
-            <p className="lg:w-full text-left  lg:text-left p-20-bold mb-2">
+            <p className="lg:w-full text-left  lg:text-left p-20-medium mb-2">
               CoinAsk
             </p>
             <p className="text-dark-minor p-16-normal text-left">
@@ -192,13 +216,13 @@ export default function Product() {
         </div>
 
         <div className="lg:flex lg:flex-wrap lg:mt-9">
-          <div className="relative bg-white w-286px sm:w-346px lg:ml-76px  m-auto pt-6 pr-7 pl-15 pb-6 shadow-lg  mb-16 lg:mb-0">
+          <div className="border border-grey-200 relative bg-white w-286px sm:w-346px lg:ml-76px  m-auto pt-6 pr-7 pl-15 pb-6 shadow-lg  mb-16 lg:mb-0">
             <img
               src={Odes}
               alt=""
-              className="w-72px p-4 shadow bg-white absolute -top-9 -left-9 "
+              className="w-72px p-4 shadow bg-white absolute -top-9 -left-9 border border-grey-200"
             />
-            <p className="lg:w-full text-left  lg:text-left p-20-bold mb-2">
+            <p className="lg:w-full text-left  lg:text-left p-20-medium mb-2">
               Odes
             </p>
             <p className="text-dark-minor p-16-normal text-left">
